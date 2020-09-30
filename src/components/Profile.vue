c0bccd06fad47cf8f59dc18c0dd0f4a863265a84
piupaulgit <contactpiu619@gmail.com>
2020-09-30 14:31:28 +0530

    profile route added

---
+++ src/components/Profile.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>This is a profile page {{ username }} j {{ name }}</h1>
+</template>
+<script>
+export default {
+  name: "profile",
+  data() {
+    return {
+      username: this.$route.params.userName,
+      name: "pop",
+    };
+  },
+  created() {
+    console.log(this.$route.params.userName);
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import VueRouter from "vue-router";
 import Front from "./components/Front";
+import Profile from "./components/Profile";
 
 Vue.use(VueRouter);
 
-const routes = [{ path: "/", component: Front }];
+const routes = [
+  { path: "/", component: Front },
+  { path: "/profile/:userName", component: Profile },
+];
 
 const router = new VueRouter({ routes });
 Vue.config.productionTip = false;
