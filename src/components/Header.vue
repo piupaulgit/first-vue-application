a89e71b5b55f7262ae8164f964e7a7ca35b09dfb
piupaulgit <contactpiu619@gmail.com>
2020-09-30 17:30:50 +0530

    header added

---
+++ src/components/Header.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="header">
+    <span class="profile-img">
+      <img v-bind:src="userInfo.avatar_url" />
+    </span>
+    <h2>
+      <a :href="userInfo.html_url" target="_blank">@{{ userInfo.login }} </a>
+    </h2>
+
+    <span class="joined"
+      ><i class="far fa-calendar-alt"></i> Joined on:
+      {{ userInfo.created_at }}</span
+    >
+    <p class="bio">{{ userInfo.bio }}</p>
+    <div class="more-info">
+      <div class="each-info">
+        <h3>{{ userInfo.public_repos }}</h3>
+        <p>REPOSITORIES</p>
+      </div>
+      <div class="each-info">
+        <h3></h3>
+        <p>FOLLOWERS</p>
+      </div>
+      <div class="each-info">
+        <h3></h3>
+        <p>FOLLOWING</p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Header",
+  props: ["userInfo"],
+  mounted() {
+    console.log(this.userInfo);
+  },
+};
+</script>
+<style>
+.header {
+  background: #0d276b;
+  padding: 100px;
+  color: #fff;
+}
+.profile-img {
+  height: 100px;
+  width: 100px;
+  border-radius: 50%;
+  background: #fff;
+  display: block;
+  margin: 0 auto;
+  overflow: hidden;
+  border: 2px solid #fff;
+  margin-bottom: 20px;
+}
+.profile-img img {
+  max-width: 100%;
+}
+h2 {
+  color: #fff;
+}
+h2 a {
+  color: #fff;
+}
+.bio {
+  padding: 15px 0;
+}
+.joined {
+  background: #060b4c;
+  padding: 6px 12px;
+  border-radius: 4px;
+  font-size: 12px;
+  margin-top: 20px;
+  display: inline-block;
+}
+.joined .far {
+  margin-right: 5px;
+}
+.more-info {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 40px;
+}
+.each-info {
+  background: #060b4c;
+  border-radius: 5px;
+  padding: 10px;
+  min-width: 200px;
+  min-height: 141px;
+  margin-right: 29px;
+}
+.each-info:last-child {
+  margin-right: 0;
+}
+</style>
--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,20 +1,28 @@
 <template>
-  <h1>This is a profile page {{ username }} j {{ name }}</h1>
+  <div class="profile-holder">
+    <Header v-bind:userInfo="userInfo"></Header>
+    <h1>This is a profile page {{ username }} j</h1>
+  </div>
 </template>
 <script>
+import Header from "./Header";
 export default {
   name: "profile",
+  components: { Header },
   data() {
     return {
       username: this.$route.params.userName,
-      name: "pop",
+      userInfo: Object,
+      userConfig: Object,
     };
   },
   created() {
     this.axios
       .get(`https://api.github.com/users/${this.username}`)
       .then((res) => {
-        console.log(res);
+        this.userInfo = res.data;
+        this.userConfig = res.config;
+        console.log(res.data, res.config);
       });
   },
 };
